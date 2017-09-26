ac0db37017137ba8d1841bd82edc7af2f96f322a
John Carlo De Leon <alolzz123@gmail.com>
2017-09-26 19:17:52 +0800

    Auth Layout

--- app/src/main/java/com/training/android/roomate/AuthenticationActivity.java
+++ app/src/main/java/com/training/android/roomate/AuthenticationActivity.java
@@ -2,12 +2,23 @@ package com.training.android.roomate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+
+    private EditText metPhoneNum;
+    private EditText metVerify;
+    private Button mbtnSignin;
+    private Button mbtnVerify;
+    private Button mbtnResend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+
     }
 }
